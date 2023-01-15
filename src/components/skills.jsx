e0361e1c1c3d4da039c5e3c14345a736925f99d3
David Johan Hasiholan Parhusip <david.johan@ui.ac.id>
2023-01-15 23:31:09 +0700

    add project section and change ui

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -23,30 +23,33 @@ class Skills extends React.Component {
     render() {
       return (
         <section id="about" className="about-mf sect-pt4 route">
-          <div className="container">
-            <div className="row">
-              <div className="col-sm-12">
-                <div className="box-shadow-full">
-                  <div className="row">
-                    <div className="col">
-                      <div className="about-me pt-4 pt-md-0">
-                        <div className="title-box-2">
-                          <h5 className="title-left">Skills</h5>
-                        </div>
-                        <ul className='skills__list'>
-                          {this.state.curSkills.map((skill) => (
-                            <li key={skill.id} className='skills__list-item btn btn--plain'>
-                              {skill}
-                            </li>
-                          ))}
-                        </ul>
-                      </div>
-                    </div>
-                  </div>
-                </div>
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12">
+              <div className="title-box text-center">
+                <h3 className="title-a">Skills</h3>
+                <p className="subtitle-a">
+                  Incididunt nostrud id aute culpa excepteur pariatur consequat
+                  elit culpa nulla enim anim incididunt.
+                </p>
+                <div className="line-mf"></div>
               </div>
             </div>
           </div>
+          <div className="row">
+            <div className="col">
+              <div className="about-me pt-4 pt-md-0">
+                  <ul className='skills__list text-white'>
+                    {this.state.curSkills.map((skill) => (
+                      <li key={skill.id} className='skills__list-item btn btn--plain'>
+                        {skill}
+                      </li>
+                    ))}
+                  </ul>
+              </div>
+            </div>
+          </div>
+        </div>
         </section>
       );
     }
